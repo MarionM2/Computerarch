0b54b89053599d9b39d26e8fb546f1d1e841eb03
marionm2 <m.maina2@alustudent.com>
2022-03-03 12:42:20 +0200

    comments

--- mips.py
+++ mips.py
@@ -9,11 +9,15 @@ Loop beq r2 r1 exit
 exit
 
     """
-
+r0 = 45
 r2 = 45 + 0
 r1 = 45 + 5
+# a loop to check if variables r2 and r1 are not equal
 while r2 != r1:
+    # if the condition is met the following operations are completed
     r6 = 30
     r6 += r2
+    # increment r2
     r2 += 1
+    # show the new output for r2
 print(r2)
